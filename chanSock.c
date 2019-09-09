8c477f94b2c49ce9f20556ec8f9ac358573259e0
G. David Butler <gdavidbutler@users.noreply.github.com>
2019-09-09 15:13:26 +0000

    use chanPoll to avoid another stack frame

--- chanSock.c
+++ chanSock.c
@@ -36,29 +36,37 @@ static void *
 chanSockC(
   void *v
 ){
-  chanSockM_t *m;
-  void *t;
   struct chanSockX *x;
+  chanSockM_t *m;
+  chanPoll_t h;
 
-  if (!chanRecv(0, v, &t))
+  h.c = v;
+  h.v = (void **)&x;
+  h.o = chanOpRecv;
+  if (!chanPoll(0, 1, &h))
     return 0;
-  x = t;
   pthread_cleanup_push((void(*)(void*))chanShut, x->w);
-  t = 0;
-  pthread_cleanup_push((void(*)(void*))x->f, t);
-  while (chanRecv(0, x->w, &t)
-   && (m = t)
+  m = 0;
+  pthread_cleanup_push((void(*)(void*))x->f, m);
+  h.c = x->w;
+  h.v = (void **)&m;
+  while (chanPoll(0, 1, &h)
    && (m->l)
    && write(x->d, m->b, m->l) == m->l) {
-    x->f(t);
-    t = 0;
+    x->f(m);
+    m = 0;
   }
-  pthread_cleanup_pop(1); /* x->f(t) */
+  pthread_cleanup_pop(1); /* x->f(m) */
+  m = 0;
   pthread_cleanup_pop(1); /* chanShut(x->w) */
   shutdown(x->d, SHUT_WR);
-  chanSend(0, v, 0);
-  while (chanRecv(1, x->w, &t))
-    x->f(t);
+  h.c = v;
+  h.o = chanOpSend;
+  chanPoll(0, 1, &h);
+  h.c = x->w;
+  h.o = chanOpRecv;
+  while (chanPoll(1, 1, &h))
+    x->f(m);
   return 0;
 }
 
@@ -66,27 +74,36 @@ static void *
 chanSockS(
   void *v
 ){
+  struct chanSockX *x;
   chanSockM_t *m;
   void *t;
-  struct chanSockX *x;
+  chanPoll_t h;
 
-  if (!chanRecv(0, v, &t))
+  h.c = v;
+  h.v = (void **)&x;
+  h.o = chanOpRecv;
+  if (!chanPoll(0, 1, &h))
     return 0;
-  x = t;
   pthread_cleanup_push((void(*)(void*))chanShut, x->r);
   m = 0;
   pthread_cleanup_push((void(*)(void*))x->f, m);
+  h.c = x->r;
+  h.v = (void **)&m;
+  h.o = chanOpSend;
   while ((m = x->a(0, sizeof(*m) + x->l - 1))
    && (int)(m->l = read(x->d, m->b, x->l)) > 0) {
     if ((t = x->a(m, sizeof(*m) + m->l)))
       m = t;
-    if (!chanSend(0, x->r, m))
+    if (!chanPoll(0, 1, &h))
       break;
   }
   pthread_cleanup_pop(1); /* x->f(m) */
+  m = 0;
   pthread_cleanup_pop(1); /* chanShut(x->r) */
   shutdown(x->d, SHUT_RD);
-  chanSend(0, v, 0);
+  h.c = v;
+  h.o = chanOpSend;
+  chanPoll(0, 1, &h);
   return 0;
 }
 
@@ -94,15 +111,18 @@ static void *
 chanSockW(
   void *v
 ){
-  void *t;
   struct chanSockX *x;
+  void *t;
   pthread_t pC;
   pthread_t pS;
+  chanPoll_t h;
 
   pthread_cleanup_push((void(*)(void*))chanFree, v);
-  if (!chanRecv(0, v, &t))
+  h.c = v;
+  h.v = (void **)&x;
+  h.o = chanOpRecv;
+  if (!chanPoll(0, 1, &h))
     goto exit0;
-  x = t;
   pthread_cleanup_push((void(*)(void*))x->f, x);
   pthread_cleanup_push((void(*)(void*))close, (void *)(long)x->d);
   pthread_cleanup_push((void(*)(void*))chanShut, x->r);
@@ -110,15 +130,18 @@ chanSockW(
   if (pthread_create(&pC, 0, chanSockC, v))
     goto exit1;
   pthread_cleanup_push((void(*)(void*))pthread_cancel, (void *)pC);
-  if (!chanSendWait(0, v, x))
+  h.o = chanOpSendWait;
+  if (!chanPoll(0, 1, &h))
     goto exit2;
   if (pthread_create(&pS, 0, chanSockS, v))
     goto exit2;
   pthread_cleanup_push((void(*)(void*))pthread_cancel, (void *)pS);
-  if (!chanSendWait(0, v, x))
+  if (!chanPoll(0, 1, &h))
     goto exit3;
-  chanRecv(0, v, &t);
-  chanRecv(0, v, &t);
+  h.v = &t;
+  h.o = chanOpRecv;
+  chanPoll(0, 1, &h);
+  chanPoll(0, 1, &h);
   pthread_join(pC, 0);
   pthread_join(pS, 0);
 exit3:
@@ -145,14 +168,14 @@ chanSock(
  ,chan_t *r
  ,chan_t *w
 ){
-  chan_t *c;
   struct chanSockX *x;
   pthread_t p;
+  chanPoll_t h;
 
   x = 0;
   if (!a || !f || !l || d < 0 || !r || !w || !(x = a(0, sizeof(*x))))
     return 0;
-  if (!(c = chanAlloc(a, f, 0, 0, 0))) {
+  if (!(h.c = chanAlloc(a, f, 0, 0, 0))) {
     f(x);
     return 0;
   }
@@ -162,15 +185,17 @@ chanSock(
   x->d = d;
   x->r = r;
   x->w = w;
-  if (pthread_create(&p, 0, chanSockW, c)) {
-    chanFree(c);
+  if (pthread_create(&p, 0, chanSockW, h.c)) {
+    chanFree(h.c);
     f(x);
     return 0;
   }
-  if (!chanSend(0, c, x)) {
+  h.v = (void **)&x;
+  h.o = chanOpSend;
+  if (!chanPoll(0, 1, &h)) {
     pthread_cancel(p);
     pthread_join(p, 0);
-    chanFree(c);
+    chanFree(h.c);
     f(x);
     return 0;
   }
